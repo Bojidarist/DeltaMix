de315c80ecc3a70f200c4ffc2b5f6fed50666d6e
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2019-10-29 21:26:30 +0200

    Add ButtonController and pressed sprites for the BoxNotes

---
+++ DeltaMix/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    /// <summary>
+    /// The sprite renderer for the current button
+    /// </summary>
+    private SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// The default sprite of the button
+    /// </summary>
+    public Sprite defaultSprite;
+
+    /// <summary>
+    /// The sprite when the button is pressed
+    /// </summary>
+    public Sprite pressedSprite;
+
+    /// <summary>
+    /// The key that corresponds to this button
+    /// </summary>
+    public KeyCode keyToPress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            spriteRenderer.sprite = pressedSprite;
+        }
+        if (Input.GetKeyUp(keyToPress))
+        {
+            spriteRenderer.sprite = defaultSprite;
+        }
+    }
+}
